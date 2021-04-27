a4c72a0150aa103abe15f17519f22467fdc19117
GCastro68 <gcastro2805@gmail.com>
2021-04-26 21:41:25 -0600

    G202104262141

--- index.js
+++ index.js
@@ -43,12 +43,9 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
     const { password, username } = req.body;
-    const hash = await bcrypt.hash(password, 12);
+    //const hash = await bcrypt.hash(password, 12);
 
-    const user = new User({
-        username,
-        password: hash
-    });
+    const user = new User({ username, password });
     await user.save();
 
     //Usuario válido, se guarda el id en variable de sesión
@@ -63,11 +60,10 @@ app.get('/login', (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    const validPassword = await bcrypt.compare(password, user.password);
 
-    if (validPassword) {
-        req.session.user_id = user._id;    //ERROR
+    const foundUser = await User.findAndValidate(username, password);
+    if (foundUser) {
+        req.session.user_id = foundUser._id;    //ERROR
         res.redirect('/secret');
     } else {
         res.redirect('/login');
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 // Usuarios      202104221150    GACC
 const mongoose = require('mongoose');
-//bcrypt = require('bcrypt');
+bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -15,11 +15,26 @@ const userSchema = new mongoose.Schema({
 
 //Se crea la función findAndValidate
 //Se pueden crear cualquiefunción después de userSchema.statics.
-// userSchema.statics.findAndValidate = async function (username, password) {
-//     const foundUser = await this.findOne({ username });
-//     const isValid = await bcrypt.compare(password, foundUser.password);
-//     return isValid ? foundUser : false;
-// }
+userSchema.statics.findAndValidate = async function (username, password) {
+    const foundUser = await this.findOne({ username });
+    const isValid = await bcrypt.compare(password, foundUser.password);
+    return isValid ? foundUser : false;
+}
+
+//Se crea un middleware para que al momento de guardar en la BD
+//Mongo cree el password encriptado, se usa .pre para que la función se ejecute
+//antes de guardar en la BD el password
+userSchema.pre('save', async function (next) {
+    // await ya que necesita tiempo, son 12 rounds, por eso se define la función async
+
+    //Si el password no ha sido modificado continua con grabar en la BD
+    if (!this.isModified('password')) return next();
+
+    //En caso de que el password haya sido modificado se calcula de nuevo
+    //la encriptación del password
+    this.password = await bcrypt.hash(this.password, 12);
+    next();    //En este caso es save
+});
 
 module.exports = mongoose.model('User', userSchema);
 
